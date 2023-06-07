0aac03849a29a276ce8a261e763473ad04b85774
GwGibson <gibsonwgraham@gmail.com>
2023-06-07 11:40:52 -0400

    Fix bug in averageArray, now works for empty array

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -10,6 +10,9 @@ fun main() {
 * @return Double - Average value as a Double
 */
 fun averageArray(inputArray: Array<Int>): Double {
+    if (inputArray.isEmpty()) { // Avoid divide by 0 in return
+        return 0.0 
+    }
     var sum = 0.0
     for (number in inputArray) {
         sum += number
@@ -21,4 +24,6 @@ fun averageArrayTester() {
     println(averageArray(arrayOf(1,2,3))) // Except: 2
     println(averageArray(arrayOf(-1,2,-3))) // Expect: -2/3
     println(averageArray(arrayOf(-1909))) // Expect: -1909
+    println(averageArray(arrayOf(-0))) // Expect: 0
+    println(averageArray(arrayOf()))
 }
