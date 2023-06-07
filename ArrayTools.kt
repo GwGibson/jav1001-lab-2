c8352d69edbca28a24f61a918ecbf4f4b3f214b9
GwGibson <gibsonwgraham@gmail.com>
2023-06-07 12:16:46 -0400

    Complete arrayContains

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,29 +1,45 @@
 fun main() {
     averageArrayTester()
+    println(arrayContains(arrayOf(1, 2, 3), 2)) // expect: true
 }
 
 /**
-* This function takes in an array of integers and 
-* calculates the average value of the numbers.
-*
-* @param inputArray - Array of Integers
-* @return Double - Average value as a Double
-*/
-fun averageArray(inputArray: Array<Int>): Double {
+ * This function takes in an array of integers and calculates the average value of the numbers.
+ *
+ * @param inputArray
+ * - Array of Integers
+ * @return Double - Average value as a Double, or 0 if inputArray is empty
+ */
+fun arrayAverage(inputArray: Array<Int>): Double {
     if (inputArray.isEmpty()) { // Avoid divide by 0 in return
-        return 0.0 
+        return 0.0
     }
     var sum = 0.0
     for (number in inputArray) {
         sum += number
     }
-    return sum/inputArray.size
+    return sum / inputArray.size
 }
 
 fun averageArrayTester() {
-    println(averageArray(arrayOf(1,2,3))) // Except: 2
-    println(averageArray(arrayOf(-1,2,-3))) // Expect: -2/3
-    println(averageArray(arrayOf(-1909))) // Expect: -1909
-    println(averageArray(arrayOf(-0))) // Expect: 0
-    println(averageArray(arrayOf()))
+    println(arrayAverage(arrayOf(1, 2, 3))) // Except: 2
+    println(arrayAverage(arrayOf(-1, 2, -3))) // Expect: -2/3
+    println(arrayAverage(arrayOf(-1909))) // Expect: -1909
+    println(arrayAverage(arrayOf(-0))) // Expect: 0
+    println(arrayAverage(arrayOf()))
+}
+
+class MyInt(var int: Int) {
+    fun equals(other: MyInt): Boolean {
+        return this.int == other.int
+    }
+}
+
+fun arrayContains(inputArray: Array<Int>, target: Int): Boolean {
+    for (element in inputArray) {
+        if (element == target) {
+            return true
+        }
+    }
+    return false
 }
